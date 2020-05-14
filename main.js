733df7df209bdbf2b4ad980be04c93245717ad6e
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-05-14 15:58:15 -0400

    styled sliders more...fixed some css bugs

---
+++ main.js
@@ -0,0 +1,15 @@
+// var barValue = document.getElementById("dPayment").innerHTML
+// var slider = document.getElementById("myRange")
+
+// console.log(barValue)
+
+
+//     if (this.slider.value){
+//     slider.addEventListener("mousemove", function(){
+//         var color =  'linear-gradient(90deg, rgb(76, 175, 80)' + barValue + '%, rgb(214,214,214)' + barValue + '%';
+//         slider.style.background = color;
+//       })
+//     }
+//     else {'this program is a jerk'}
+ 
+  
